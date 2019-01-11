055e161db46d9cacfb87b7f4656900743fe6a2fb
han77x <32688245+han77x@users.noreply.github.com>
2019-01-10 22:04:17 -0800

    Add files via upload

---
+++ lab2/script.js
@@ -0,0 +1,12 @@
+function addTwoNumbers() {
+   	var a = Number(prompt("Enter first number for Sum1"));
+    var b = Number(prompt("Enter second number for Sum1"));
+alert("Sum 1 = " + (a + b));
+    var x = Number(prompt("Enter first number for Sum2"));
+    var y = Number(prompt("Enter first number for Sum2"));
+alert("Sum 2 = " + (x + y));
+alert("Yay! You can now enter!")
+	
+}
+
+addTwoNumbers();
